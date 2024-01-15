0309ee94a95638a24bbc1375a0d4e04236366446
beeplaced <you@example.com>
2024-01-15 15:13:16 +0100

    up

---
+++ cronService.js
@@ -0,0 +1,47 @@
+const cron = require('node-cron');
+const axios = require('axios');
+const fs = require('fs');
+
+const configFile = 'cronConfig.json';
+
+const readConfigFile = () => {
+    try {
+        const data = fs.readFileSync(configFile, 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        console.error('Error reading config file:', error.message);
+        return null;
+    }
+};
+
+const executeApiRequest = async (apiEndpoint, serviceName, apiKey) => {
+    try {
+        const response = await axios.get(apiEndpoint, {
+            headers: {
+                'x-api-key': apiKey,
+            },
+        });
+
+        console.log(`${serviceName} - API response:`, response.data);
+    } catch (error) {
+        console.error(`${serviceName} - Error making API request:`, error.message);
+    }
+};
+
+const startCronJobs = () => {
+    const config = readConfigFile();
+
+    if (config && config.tasks) {
+        config.tasks.forEach((task) => {
+            const { service, endpoint, schedule, active, apiKey } = task;
+            if (!active) return
+            cron.schedule(schedule, () => {
+                executeApiRequest(endpoint, service, apiKey);
+            });
+        });
+    } else {
+        console.log('Invalid or missing configuration.');
+    }
+};
+
+startCronJobs();
